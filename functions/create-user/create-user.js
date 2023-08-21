6ead611c6c3a53eb46c8d0d7f2f7bc85c4916f6f
jaffkeras <you@example.com>
2023-08-21 12:18:39 +0530

    fuck this is more pain

--- functions/create-user/create-user.js
+++ functions/create-user/create-user.js
@@ -1,31 +1,39 @@
 const fetch = require('node-fetch')
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient("");
+
 
 const handler = async (event, context) => {
   if (event.httpMethod !== 'POST') return { statusCode: 400, body: 'Must POST to this function' }
 
-  // send account information along with the POST
-  const { email, full_name: fullName, password } = JSON.parse(event.body)
-  if (!email) return { statusCode: 400, body: 'email missing' }
-  if (!password) return { statusCode: 400, body: 'password missing' }
-  if (!fullName) return { statusCode: 400, body: 'full_name missing' }
+  const realpath = event.path.replace('/.netlify/functions/', '').replace(/\//gim, '')
+  
+    if (realpath === 'create-user') {
+    // send account information along with the POST
+    const { email, full_name: fullName, password } = JSON.parse(event.body)
+    if (!email) return { statusCode: 400, body: 'email missing' }
+    if (!password) return { statusCode: 400, body: 'password missing' }
+    if (!fullName) return { statusCode: 400, body: 'full_name missing' }
 
-  // identity.token is a short lived admin token which
-  // is provided to all Netlify Functions to interact
-  // with the Identity API
-  const { identity } = context.clientContext
+    // identity.token is a short lived admin token which
+    // is provided to all Netlify Functions to interact
+    // with the Identity API
+    const { identity } = context.clientContext
 
-  await fetch(`${identity.url}/admin/users`, {
-    method: 'POST',
-    headers: { Authorization: `Bearer ${identity.token}` },
-    body: JSON.stringify({
-      email,
-      password,
-      confirm: true,
-      user_metadata: {
-        full_name: fullName,
-      },
-    }),
-  })
+    await fetch(`${identity.url}/admin/users`, {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${identity.token}` },
+      body: JSON.stringify({
+        email,
+        password,
+        confirm: true,
+        user_metadata: {
+          full_name: fullName,
+        },
+      }),
+    })
+  }
 
   return {
     statusCode: 200,
